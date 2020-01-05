6755cae9ae1a4ea9e9970abfb72e564cb5b6d7ba
fedorovse <ingor-ru@mail.ru>
2020-01-05 11:30:35 +0700

    4.5. Отладка программы в IDEA[#215440]

--- chapter_001/src/main/java/ru/job4j/condition/SqMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqMax.java
@@ -21,20 +21,4 @@ public class SqMax {
         }
         return result;
     }
-
-
-
-
-
-   // public static int max(int first, int second, int third, int forth) {
-   //     int result = forth;
-    //    if (first > second && first > third && first > forth) {
-    //        result = first;
-    //    } else if (second > third && second > forth) {
-    //        result = second;
-    //    } else if (third > forth) {
-    //        result = third;
-     //   }
-    //    return result;
-   // }
 }
