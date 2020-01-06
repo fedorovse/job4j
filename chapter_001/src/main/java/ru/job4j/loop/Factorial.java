e4848a99064e679ded2482051e4f92b8ddf6d088
fedorovse <ingor-ru@mail.ru>
2020-01-06 21:33:58 +0700

    5.3. Построить шахматную доску в псевдографике.[#215515] Исправление1

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -4,13 +4,9 @@ public class Factorial {
 
     public int calc(int n) {
         int result = 1;
-        if(n == 0 || n == 1) {
-            return result;
-        } else {
-            for (int index = 1; index <= n; index++) {
-                result = result * index;
-            }
-            return result;
+        for (int index = 1; index <= n; index++) {
+            result = result * index;
         }
+        return result;
     }
 }
