bf5aff671f7dc52f32133a7abba32ac970d71ecf
fedorovse <ingor-ru@mail.ru>
2020-01-07 00:10:56 +0700

    5.6. Простые числа.[#215507]

---
+++ chapter_001/src/test/java/ru/job4j/loop/PrimeNumberTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class PrimeNumberTest {
+
+    @Test
+    public void when5() {
+        PrimeNumber prime = new PrimeNumber();
+        int count = prime.calc(5);
+        assertThat(3, is(count));
+    }
+
+    @Test
+    public void when11() {
+        PrimeNumber prime = new PrimeNumber();
+        int count = prime.calc(11);
+        assertThat(5, is(count));
+    }
+
+    @Test
+    public void when2() {
+        PrimeNumber prime = new PrimeNumber();
+        int count = prime.calc(2);
+        assertThat(1, is(count));
+    }
+
+    @Test
+    public void when1() {
+        PrimeNumber prime = new PrimeNumber();
+        int count = prime.calc(1);
+        assertThat(0, is(count));
+    }
+}
