856286dc1eed54db4245cb7b3f0562608ad859ef
fedorovse <ingor-ru@mail.ru>
2020-01-21 21:46:52 +0700

    2.1. Рефакторинг - Расстояние между точками.[#215590]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,17 +1,48 @@
 package ru.job4j.condition;
 
+/**
+ * class Point вычисления в декартовой системе координат
+ * @author Sergey Fedorov (ingor-ru@mail.ru)
+ * @version 2
+ */
 public class Point {
 
-    public static double distance(int x1, int y1, int x2, int y2) {
-        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    /**
+     * x - проекция точки на ось Х в декартовой системе координат
+     */
+    private int x;
+    /**
+     * у - проекция точки на ось У в декартовой системе координат
+     */
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * method distance - вычисляет расстояние между точками в декартовой системе координат. Первая точка - текущий
+     * объект Point
+     * @param that Point - объект содержащий коорднаты второй точки.
+     * @return double - расстояние между точками.
+     */
+    public double distance(Point that) {
+        return Math.sqrt(Math.pow((that.x - this.x), 2) + Math.pow((that.y - this.y), 2));
     }
 
     public static void main(String[] args) {
-        double result = distance(0, 0, 0, 2);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double result = a.distance(b);
         System.out.println(" result (0,0) to (0,2) " + result);
-        result = distance(5, 0, 3, 0);
+        Point c = new Point(5, 0);
+        Point d = new Point(3, 0);
+        result = c.distance(d);
         System.out.println(" result (5,0) to (3,0) " + result);
-        result = distance(8, 3, 6, 5);
+        Point e = new Point(8, 3);
+        Point f = new Point(6, 5);
+        result = e.distance(f);
         System.out.println(" result (8,3) to (6,5) " + result);
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -9,10 +9,12 @@ public class PointTest {
     public void distance1() {
         int inX1 = 8;
         int inY1 = 3;
+        Point a = new Point(inX1, inY1);
         int inX2 = 6;
         int inY2 = 5;
+        Point b = new Point(inX2, inY2);
         double expected = 2.828427;
-        double out = Point.distance(inX1, inY1, inX2, inY2);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 
@@ -20,10 +22,12 @@ public class PointTest {
     public void distance2() {
         int inX1 = 0;
         int inY1 = 0;
+        Point a = new Point(inX1, inY1);
         int inX2 = 2;
         int inY2 = 0;
+        Point b = new Point(inX2, inY2);
         double expected = 2.0;
-        double out = Point.distance(inX1, inY1, inX2, inY2);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 }
