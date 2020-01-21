61c36e7daf061559839001d02000c3b0f04797da
fedorovse <ingor-ru@mail.ru>
2020-01-22 00:16:14 +0700

    2.2. Рефакторинг - Площадь треугольника.[#215591]

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -1,8 +1,62 @@
 package ru.job4j.condition;
 
+/**
+ *
+ */
 public class Triangle {
 
-    public static boolean exist(double ab, double ac, double bc) {
-        return ab + ac > bc && ab + bc > ac && ac + bc > ab;
+    /**
+     * поля first, second, third - это объекты Point с координатами в декартовой системе координат
+     */
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+    /**
+     * Расчитывает площадь треугольника если это возможно
+     * @return double - площадь треугольника, если невозможно рассчитать, то -1
+     */
+    public double area() {
+        double result = -1;
+        double a = first.distance(second);
+        double b = first.distance(third);
+        double c = second.distance(third);
+        if (this.exist(a, b, c)) {
+            double halfP = this.period(a, b, c);
+            result = Math.sqrt(halfP * (halfP - a) * (halfP - b) * (halfP - c));
+        }
+        return  result;
+    }
+
+    /**
+     * method period - вычисляет полупериметр треугольнико образованного сторонами a, b, c.
+     * @param a - расстояние между точками ab
+     * @param b - расстояние между точками ac
+     * @param c - расстояние между точками bc
+     * @return
+     */
+    public double period(double a, double b, double c) {
+        if (exist(a, b, c)) {
+            return (a + b + c) / 2;
+        } else {
+            return  -1;
+        }
+    }
+
+    /**
+     * method exist - проверяет возможно ли построить треугольник с такими сторонами
+     * @param a - расстояние между точками ab
+     * @param b - расстояние между точками ac
+     * @param c - расстояние между точками bc
+     * @return true - если треугольник построить возможно и false - если нет.
+     */
+    private boolean exist(double a, double b, double c) {
+        return a + b > c && a + c > b && b + c > a;
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.condition;
 
+import org.junit.Assert;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -7,14 +8,24 @@ import static org.junit.Assert.assertThat;
 public class TriangleTest {
 
     @Test
-    public void whenExist() {
-        boolean result = Triangle.exist(2.0, 2.0, 2.0);
-        assertThat(result, is(true));
+    public void whenAreaTrue() {
+        Point a = new Point(2, 0);
+        Point b = new Point(0, 0);
+        Point c = new Point(0, 2);
+        Triangle inputTriangle = new Triangle(a, b, c);
+        double result = inputTriangle.area();
+        double expect = 2;
+        Assert.assertEquals(expect, result, 0.01);
     }
 
     @Test
-    public void whenNotExist() {
-        boolean result = Triangle.exist(5.0, 2.0, 2.0);
-        assertThat(result, is(false));
+    public void whenAreaFalse() {
+        Point a = new Point(5, 0);
+        Point b = new Point(0, 0);
+        Point c = new Point(2, 0);
+        Triangle inputTriangle = new Triangle(a, b, c);
+        double result = inputTriangle.area();
+        double expect = -1;
+        Assert.assertEquals(expect, result, 0.01);
     }
 }
