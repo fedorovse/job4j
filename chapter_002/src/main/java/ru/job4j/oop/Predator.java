69e5b7dabcf6224f182bcb2e2eac6ca3329d2481
fedorovse <ingor-ru@mail.ru>
2020-01-22 23:39:48 +0700

    1. Конструктор по умолчанию.[#215605]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Predator.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+public class Predator extends Animal {
+
+    public Predator() {
+        super();
+        System.out.println("Class Predator");
+    }
+
+    public static void main(String[] args) {
+        Predator predator = new Predator();
+    }
+}
