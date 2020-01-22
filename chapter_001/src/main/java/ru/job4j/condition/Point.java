8d26cfd5e1169f14401eeb5bed59bc2875ad0a64
fedorovse <ingor-ru@mail.ru>
2020-01-22 22:42:56 +0700

    4. Расстояние между точками в трехмерном пространстве.[#215592]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -15,10 +15,21 @@ public class Point {
      * у - проекция точки на ось У в декартовой системе координат
      */
     private int y;
+    /**
+     * z - проекция точки на ось z в декартовой системе координат
+     */
+    private int z;
 
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
+        this.z = 0;
+    }
+
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 
     /**
@@ -31,6 +42,32 @@ public class Point {
         return Math.sqrt(Math.pow((that.x - this.x), 2) + Math.pow((that.y - this.y), 2));
     }
 
+    /**
+     * method distance3d - возвращает расстояние между точками в трехмерной декартовой системе координат
+     * @param that Point - объект содержащий координаты второй точки
+     * @return double - расстояние между точками
+     */
+    public double distance3d(Point that) {
+        return Math.sqrt(Math.pow((that.x - this.x), 2)
+                + Math.pow((that.y - this.y), 2)
+                + Math.pow((that.z - this.z), 2)
+        );
+    }
+
+    /**
+     * Выводит в консоль координаты точки на плоскости
+     */
+    public void info() {
+        System.out.println(String.format("Point[%s, %s]", this.x, this.y));
+    }
+
+    /**
+     * Выводит в консоль координаты точки в трехмерной системе координат
+     */
+    public void info3d() {
+        System.out.println(String.format("Point[%s, %s, %s]", this.x, this.y, this.z));
+    }
+
     public static void main(String[] args) {
         Point a = new Point(0, 0);
         Point b = new Point(0, 2);
@@ -38,11 +75,15 @@ public class Point {
         System.out.println(" result (0,0) to (0,2) " + result);
         Point c = new Point(5, 0);
         Point d = new Point(3, 0);
+        c.info();
+        d.info();
         result = c.distance(d);
         System.out.println(" result (5,0) to (3,0) " + result);
-        Point e = new Point(8, 3);
-        Point f = new Point(6, 5);
-        result = e.distance(f);
-        System.out.println(" result (8,3) to (6,5) " + result);
+        Point e = new Point(8, 3, 5);
+        Point f = new Point(6, 5, 4);
+        e.info3d();
+        f.info3d();
+        result = e.distance3d(f);
+        System.out.println(" result (8,3,5) to (6,5,4) " + result);
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -30,4 +30,19 @@ public class PointTest {
         double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
+
+    @Test
+    public void whenDistance3dThen3() {
+        int inX1 = 8;
+        int inY1 = 3;
+        int inZ1 = 5;
+        Point a = new Point(inX1, inY1, inZ1);
+        int inX2 = 6;
+        int inY2 = 5;
+        int inZ2 = 4;
+        Point b = new Point(inX2, inY2, inZ2);
+        double expected = 3.0;
+        double out = a.distance3d(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
 }
