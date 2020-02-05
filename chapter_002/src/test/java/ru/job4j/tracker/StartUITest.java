88583861eb54f7b1b08ccd51b85b520bc9c9537f
fedorovse <ingor-ru@mail.ru>
2020-02-06 00:03:24 +0700

    6. Manifest для проекта Tracker[#215579]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -2,7 +2,7 @@ package ru.job4j.tracker;
 
 import org.junit.Assert;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.nullValue;
 
