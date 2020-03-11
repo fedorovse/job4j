0b4e91533bd18c8b41ae797760781b3d518f1498
fedorovse <ingor-ru@mail.ru>
2020-03-11 18:55:31 +0700

    1. JaCoCo. Процент покрытие тестами.[#215525]

--- chapter_002/src/main/java/ru/job4j/sort/Machine.java
+++ chapter_002/src/main/java/ru/job4j/sort/Machine.java
@@ -3,16 +3,16 @@ package ru.job4j.sort;
 import java.util.Arrays;
 
 public class Machine {
-    private final int[] COINS = {10, 5, 2, 1};
+    private final int[] coins = {10, 5, 2, 1};
 
     public int[] change(int money, int price) {
         int[] rsl = new int[100];
         int size = 0;
         int change = money - price;
-        for (int index = 0; index < COINS.length; index++) {
-            while (change >= COINS[index]) {
-                change -= COINS[index];
-                rsl[size] = COINS[index];
+        for (int index = 0; index < coins.length; index++) {
+            while (change >= coins[index]) {
+                change -= coins[index];
+                rsl[size] = coins[index];
                 size++;
             }
         }
