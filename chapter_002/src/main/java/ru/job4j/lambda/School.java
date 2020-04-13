48ccee59a72d854811656c71b0acfd3a18cd13c2
fedorovse <ingor-ru@mail.ru>
2020-04-14 00:37:06 +0700

    4. Преобразование List в Map.[#215416]

--- chapter_002/src/main/java/ru/job4j/lambda/School.java
+++ chapter_002/src/main/java/ru/job4j/lambda/School.java
@@ -2,6 +2,8 @@ package ru.job4j.lambda;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -9,5 +11,14 @@ public class School {
 
     List<Student> collect(List<Student> students, Predicate<Student> predict) {
         return students.stream().filter(predict).collect(Collectors.toList());
+    }
+
+    Map<Integer, Student> collect(List<Student> students) {
+        Map<Integer, Student> result = students.stream()
+                .collect(Collectors.toMap(
+                        Student::getScore,
+                        student -> student)
+                );
+        return  result;
     }
 }
--- chapter_002/src/test/java/ru/job4j/lambda/SchoolTest.java
+++ chapter_002/src/test/java/ru/job4j/lambda/SchoolTest.java
@@ -3,7 +3,10 @@ package ru.job4j.lambda;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
@@ -57,6 +60,24 @@ public class SchoolTest {
         List<Student> expect = Arrays.asList(
                 new Student(38)
         );
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenStudentToMap() {
+        School school = new School();
+        List<Student> in = List.of(
+                new Student(0),
+                new Student(50),
+                new Student(38),
+                new Student(66)
+        );
+        Map<Integer, Student> result = school.collect(in);
+        Map<Integer, Student> expect = new HashMap<>();
+        expect.put(0, new Student(0));
+        expect.put(50, new Student(50));
+        expect.put(38, new Student(38));
+        expect.put(66, new Student(66));
         assertThat(result, is(expect));
     }
 }
