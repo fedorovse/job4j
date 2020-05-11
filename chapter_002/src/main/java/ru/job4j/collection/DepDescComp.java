83c675bbd28b82d725c28fa78ef8a26ac68fcb3b
fedorovse <ingor-ru@mail.ru>
2020-05-12 00:23:06 +0700

    Отсортировать департаменты[#215533]_правка1

--- chapter_002/src/main/java/ru/job4j/collection/DepDescComp.java
+++ chapter_002/src/main/java/ru/job4j/collection/DepDescComp.java
@@ -8,22 +8,10 @@ public class DepDescComp implements Comparator<String> {
         int rsl = 0;
         String[] o1Strings = o1.split("/");
         String[] o2Strings = o2.split("/");
-        if (!o1Strings[0].equals(o2Strings[0])) {
-            rsl = o2Strings[0].compareTo(o1Strings[0]);
+        if (o2Strings[0].compareTo(o1Strings[0]) == 0) {
+            rsl = o1.compareTo(o2);
         } else {
-            String o1Str = "";
-            String o2Str = "";
-            if (o1Strings.length > 1) {
-                for (int index = 1; index < o1Strings.length; index++) {
-                    o1Str = o1Str + "/" + o1Strings[index];
-                }
-            }
-            if (o2Strings.length > 1) {
-                for (int index = 1; index < o2Strings.length; index++) {
-                    o2Str = o2Str + "/" + o2Strings[index];
-                }
-            }
-            rsl = o1Str.compareTo(o2Str);
+            rsl = o2Strings[0].compareTo(o1Strings[0]);
         }
         return rsl;
     }
